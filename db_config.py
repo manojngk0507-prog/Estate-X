8fa66dd8a02ccfb5d6256ac96cac8d15235e7171
Manoj <manojngk0507@gmail.com>
2026-04-01 18:43:23 +0530

    added db port fix

--- db_config.py
+++ db_config.py
@@ -11,6 +11,7 @@ def get_db_connection():
         host=os.getenv("DB_HOST", "localhost"),
         user=os.getenv("DB_USER", "root"),         
         password=os.getenv("DB_PASSWORD", "system"), 
-        database=os.getenv("DB_NAME", "estate_x")
+        database=os.getenv("DB_NAME", "estate_x"),
+        port=int(os.getenv("DB_PORT", 3306)) 
     )
     return connection
